30fb34a50cfde78e298a0e5bc7920a63e33cfd1a
DishaTyagi <omgdisha@gmail.com>
2020-06-26 21:07:53 +0530

    setting 404 & Linking between routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
@@ -28,13 +28,34 @@ const helpPage = () => (
     </div>
 )
 
+const notFoundPage = () => (
+    <div>
+        404! - <Link to="/">Go Home</Link>
+    </div>
+)
+
+const Header = () => (
+    <header>
+        <h1>expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
+)
+
 const routes = (
     <BrowserRouter>     
         <div>
-            <Route path="/" component={expenseComponent} exact={true}/>
-            <Route path="/create" component={addExpensePage}/>
-            <Route path="/edit" component={editExpensePage}/>
-            <Route path="/help" component={helpPage}/>
+            <Header />
+            <Switch>
+                <Route path="/" component={expenseComponent} exact={true}/>
+                <Route path="/create" component={addExpensePage}/>
+                <Route path="/edit" component={editExpensePage}/>
+                <Route path="/help" component={helpPage}/>
+                <Route component={notFoundPage}/>
+            </Switch>
+
         </div>
     </BrowserRouter>
 );
