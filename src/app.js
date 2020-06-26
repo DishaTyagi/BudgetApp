6c89a84a527477949eb7210616d90e0d8b679037
DishaTyagi <omgdisha@gmail.com>
2020-06-26 18:32:05 +0530

    react-router 101

--- src/app.js
+++ src/app.js
@@ -6,21 +6,35 @@ import './styles/styles.scss'
 
 const expenseComponent = () => (
     <div>
-        hi! i am expense Component
+        hi! i am expense Dashboard Component
     </div>
 );
 
 const addExpensePage = () => (
     <div>
-        hi! i am expense page
+        hi! i am add expense component
     </div>
 );
+
+const editExpensePage = () => (
+    <div>
+        hi! this is edit expense component
+    </div>
+)
+
+const helpPage = () => (
+    <div>
+        This is the helpPage component.
+    </div>
+)
 
 const routes = (
     <BrowserRouter>     
         <div>
-            <Route path="/" component={expenseComponent} />
+            <Route path="/" component={expenseComponent} exact={true}/>
             <Route path="/create" component={addExpensePage}/>
+            <Route path="/edit" component={editExpensePage}/>
+            <Route path="/help" component={helpPage}/>
         </div>
     </BrowserRouter>
 );
