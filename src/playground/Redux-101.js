4d47857ef46c457a12d7933dd1c815065ca55b41
DishaTyagi <omgdisha@gmail.com>
2020-07-01 16:50:48 +0530

    Actions and store dispatching

--- src/playground/Redux-101.js
+++ src/playground/Redux-101.js
@@ -1,15 +1,22 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count : 2 }, action ) => {     //default state and its value count : 0 specified within.
-
+const store = createStore((state = { count : 0 }, action ) => {     //default state and its value count : 0 specified within.
+//the 2nd argument action contains the dispatched object from the store to modify the state values accordingly.
     switch (action.type){
         case 'INCREMENT' : 
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+
             return {        //this is the state object with credentials to be modified.
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT' :
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return{
-                count: state.count - 1
+                count: state.count - decrementBy
+            }
+        case 'SET':
+            return{
+                count: action.count
             }
         case 'RESET':
             return{
@@ -20,21 +27,34 @@ const store = createStore((state = { count : 2 }, action ) => {     //default st
     
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {     //return value of subscribe() is called to unsubscribe from the subscriptions.
+    console.log(store.getState());
+})
 
-//increment, decrement, reset -> using Actions(it is an object that is sent to the store)
 store.dispatch({        //goes to the store to compute new values of states.
     type: 'INCREMENT',
-
+    incrementBy: 5
 });
-console.log(store.getState());
+
+store.dispatch({        //goes to the store to compute new values of states.
+    type: 'INCREMENT',
+});
+// unsubscribe();
 
 store.dispatch({
     type: 'DECREMENT',
+    decrementBy: 3
 });
-console.log(store.getState());
+
+store.dispatch({
+    type: 'DECREMENT',
+})
 
 store.dispatch({
     type: 'RESET',
 })
-console.log(store.getState());
+
+store.dispatch({
+    type: 'SET',
+    count: 101
+})
