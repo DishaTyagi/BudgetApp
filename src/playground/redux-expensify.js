4fcc3c83023d2efd14ab3f2839331ce29b5a5f65
DishaTyagi <omgdisha@gmail.com>
2020-07-02 18:33:37 +0530

    Multiple reducers in Store

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,4 +1,49 @@
-import { createstore, combineReducers} from 'redux';
+import { createStore, combineReducers} from 'redux';
+
+//actions to be performed -> Action Generators.
+//ADD_EXPENSE
+//REMOVE_EXPENSE
+//EDIT_EXPENSE
+//SET_TEXT_FILTER
+//SORT_BY_DATE
+//SORT_BY_AMOUNT
+//SET_START_DATE
+//SET_END_DATE
+
+//2 reducers. 1 -> expenses array of objects and 2 -> filters.
+//Expenses reducer
+const expensesReducerDefaultState = [];
+const expensesReducer = ( state = expensesReducerDefaultState , action) => {
+    switch(action.type){
+        default: 
+            return state;
+    }
+};
+
+//Filter reducer
+const filterReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+}
+const filterReducer = (state = filterReducerDefaultState, action) => {
+    switch (action.type){
+        default:
+            return state;
+    }
+}
+
+//store
+
+const store = createStore(
+    combineReducers({       //takes obj as an argument 
+        expenses: expensesReducer,
+        filters: filterReducer
+    })
+);
+
+console.log(store.getState());
 
 const demoState = {
     expenses: [{    //array of objects
